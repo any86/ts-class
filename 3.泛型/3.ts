f5482ee1c8f135bb1b296356e46166626621b3a5
宁宁 <383514580@qq.com>
2019-07-11 17:29:43 +0800

    bak

--- 3.泛型/3.ts
+++ 3.泛型/3.ts
@@ -1,9 +1,4 @@
-
-function convert<T>(input:T):T{
-    return input;
+function echo<T>(array: T&{length:number}): T {
+    console.log(array+1); // 报错, T此时不仅代表数组, 如果非数组是没有length属性的
+    return array;
 }
-interface Convert {
-    <T>(input:T):T
-}
-
-let a:Convert = convert
