7d163a2ef4c1cc5ca6d5e5bc04b26c592afccac5
宁宁 <383514580@qq.com>
2019-07-12 10:31:33 +0800

    :zap: 完成第三课

--- 3.泛型/3.ts
+++ 3.泛型/3.ts
@@ -1,3 +1,9 @@
-function convert<T>(input:T[]):number{
-    return input.length;
+interface Goods<T>{
+    id:number;
+    title: string;
+    size: T;
 }
+
+
+let apple:Goods<string> = {id:1,title: '苹果', size: 'large'};
+let shoes:Goods<number> = {id:1,title: '苹果', size: 43};
