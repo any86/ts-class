7939bf266e060fd52a40a69b18d8665526fa644e
383514580 <383514580@qq.com>
2019-07-15 21:17:38 +0800

    fix: 修正implements部分示例错误

--- 2.基础类型和入门高级类型/2.ts
+++ 2.基础类型和入门高级类型/2.ts
@@ -1,24 +1,24 @@
 interface Animate {
-    head:number;
-    body:number;
-    foot:number;
-    eat(food:string):void;
-    say(word:string):string;
+    head: number;
+    body: number;
+    foot: number;
+    eat(food: string): void;
+    say(word: string): string;
 }
 
-class Dog implements Animate{
-    head=1;
-    body=1;
-    foot=1;
-    
-    eat(food){
+class Dog implements Animate {
+    head = 1;
+    body = 1;
+    foot = 1;
+
+    eat(food:string) {
         console.log(food);
     }
 
-    say(word){
+    say(word:string) {
         return word;
     }
 }
 
-enum Color {Red, Green, Blue};
-const b:object = Color;
+enum Color { Red, Green, Blue };
+const b: object = Color;
