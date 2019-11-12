bcb41cc852310c8ec8208a8b9147e76c1d5a9e9c
宁宁 <383514580@qq.com>
2019-11-12 17:30:58 +0800

    bak

---
+++ 6.如何写声明文件(declare)/6.d.ts
@@ -0,0 +1,6 @@
+// declare var __DEV__: boolean
+// declare var __TEST__: boolean
+// declare var __BROWSER__: boolean
+// declare var __RUNTIME_COMPILE__: boolean
+// declare var __COMMIT__: string
+// declare var __VERSION__: string
